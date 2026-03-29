087326a3951c106b0bbf66b10593884cd079ed74
Pratik Kumar Sahoo <pratikkumarsahoo5@gmail.com>
2026-03-29 10:59:33 +0530

    backend deployement issue tracker

--- issue-tracker-backend/src/main/java/com/issuetracker/IssueTrackerBackendApplication.java
+++ issue-tracker-backend/src/main/java/com/issuetracker/IssueTrackerBackendApplication.java
@@ -3,11 +3,9 @@ package com.issuetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableAsync
-@EnableJpaAuditing
 public class IssueTrackerBackendApplication {
 
 	public static void main(String[] args) {
